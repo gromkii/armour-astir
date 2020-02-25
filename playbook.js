ae16a0f2c0a1943ace6679363777d4de99a229b7
Dax <limeside@gmail.com>
2020-02-24 20:07:15 -0600

    Add move template. Not working

--- playbook.js
+++ playbook.js
@@ -35,17 +35,15 @@
     }
 */
 
-var playbookMoves = {
+var playbookMoves = { // basic moves
     weatherTheStorm: {
-        book: 'basic',
-        fullName: 'Weather The Storm (DEFY)',
+        fullName: 'Weather The Storm',
         trigger: 'When you <b>weather the storm</b> to do something safely under pressure.',
         rolls: ['DEFY', 'KNOW'],
         success: 'You mange to make it to safety.',
         partial: 'You succed, but at some cost - your Director will ask you to settle for less, take a <i>risk</i>, or make a difficult choice.',
     },
     readTheRoom: {
-        book: 'basic',
         fullName: 'Read The Room',
         trigger: 'When you <b>read the room</b> to get insight on your situation.',
         roll: 'SENSE', // ! Single roll, automatically does it, or prompt for bonus?
@@ -55,7 +53,6 @@ var playbookMoves = {
         details: 'Spend your hold 1-for-1 to ask the following questions. Your hold lasts until you leave the current situation or it changes significantly. \n\n • Who has the upper hand here?\n • What is being overlooked here?\n • How does x really feel?\n • What are x\'s real intentions?\n • How is x at risk or in peril? \n\n Roll with Advantage when you act on the answers to what you\'ve asked',
     },
     dispelUncertainties: {
-        book: 'basic',
         fullName: 'Dispel Uncertainties',
         trigger: 'When you <b>dispel uncertainties</b> by clarifying something unknown or answering a question.',
         roll: 'KNOW',
@@ -63,7 +60,6 @@ var playbookMoves = {
         partial: 'The Director will tell you something potentially useful, but it is up to you to discern how. The Director might ask you to explain how you know that information, or where you learned it.',
     },
     helpOrHinder: {
-        book: 'basic',
         fullName: 'Help or Hinder',
         trigger: 'When you <b>help or hinder</b> someome to influence their attemps to do something.',
         roll: 'GRAVITY', // ! This one might be tricky to handle.
@@ -71,7 +67,6 @@ var playbookMoves = {
         partial: 'As above, but you become entangled in the consequences of their actions, and possibly cause them.'
     },
     weaveMagic: {
-        book: 'basic',
         fullName: 'Weave Magic',
         trigger: 'When you <b>weave magic</b> to do something taxing with your power.',
         roll: 'CHANNEL',
@@ -79,39 +74,28 @@ var playbookMoves = {
         partial: 'You succeed, but your invocation is twisted in an unexpected and dangerous way.'
     },
     coolOff: {
-        book: 'basic',
-        fullName: 'coolOff',
+        fullName: 'Cool Off',
         trigger: 'When you take a moment in safety to <b>cool off</b> or help someone else do the same, you declare a <i>risk</i> you want to get rid of.',
-        roll: ['DEFY', 'SENSE', 'KNOW', 'CLASH', 'TALK' ],
+        rolls: ['DEFY', 'SENSE', 'KNOW', 'CLASH', 'TALK' ],
         success: 'You/they erase a <i>risk</i> or untick \'overheating\' from an Astir.',
         partial: 'As above, but your moment of safety is interrupted.'
     },
     exchangeBlows: {
-        book: 'basic',
         fullName: 'Exchange Blows',
         rolls: ['CLASH', 'TALK'],
         trigger: 'When you <b>exchange blows</b> with foes capable of defending themselves, and advance a GRAVITY clock if you have one.',
         success: 'Either your opponent takes a <i>risk</i>, or you take a <i>risk</i> and put your opponent in <i>peril.</i>',
         partial: 'Both you and your target are forced to take a <i>risk</i>',
     },
-    strikeDecisivelyClash: {
-        book: 'basic',
-        fullName: 'Strike Decisively (CLASH)',
-        trigger: 'When you <b>strike decisively</b> against someone wwho is defenseless.',
-        success: 'You strike true. Director characters are killed, forced to retreat, or otherwise removed as a threat as per the fiction. Player characters should <b>bite the dust</b>.',
-        partial: 'You succeed as above, but choose 1.',
-        details: '• You overreach or underestimate - take a <i>risk</i>. \n •You waste ammo or words, losing use of a weapon until you can re-arm, or losing the weight of some bargaining chip or piece of leverage. \n • You strike carelessly, causing colateral damage beyond your expectations.'
-    },
-    strikeDecisivelyTalk: {
-        book: 'basic',
-        fullName: 'Strike Decisively (TALK)',
+    strikeDecisively: {
+        fullName: 'Strike Decisively',
+        rolls: ['CLASH', 'TALK'],
         trigger: 'When you <b>strike decisively</b> against someone wwho is defenseless.',
         success: 'You strike true. Director characters are killed, forced to retreat, or otherwise removed as a threat as per the fiction. Player characters should <b>bite the dust</b>.',
         partial: 'You succeed as above, but choose 1.',
         details: '• You overreach or underestimate - take a <i>risk</i>. \n •You waste ammo or words, losing use of a weapon until you can re-arm, or losing the weight of some bargaining chip or piece of leverage. \n • You strike carelessly, causing colateral damage beyond your expectations.'
     },
     biteTheDust: {
-        book: 'basic',
         fullName: 'Bite the Dust',
         trigger: 'When you\'re defenseless or risk harm so sever you might <b>bite the dust</b>.',
         roll: 'DEFY',
